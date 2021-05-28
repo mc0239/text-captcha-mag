1816875bceacffa28ba28861bca31a101456b5d7
mc0239 <martin.cebular@gmail.com>
2021-05-28 16:30:58 +0200

    Cleanup files in chrome-extension

--- frontend/chrome-extension/background.js
+++ frontend/chrome-extension/background.js
@@ -4,15 +4,3 @@ function silence() {
 }
 
 chrome.runtime.onInstalled.addListener(silence);
-
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [
-                new chrome.declarativeContent.PageStateMatcher({ pageUrl: { hostEquals: 'developer.chrome.com' } }),
-                new chrome.declarativeContent.PageStateMatcher({ pageUrl: { hostContains: 'rtvslo.si' } }),
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});

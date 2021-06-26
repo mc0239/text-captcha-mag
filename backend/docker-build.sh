480c88e88ec8fcf533f9d02563cc1a9a5186e975
mc0239 <martin.cebular@gmail.com>
2021-06-26 16:43:18 +0200

    backend & api: update configs

--- backend/docker-build.sh
+++ backend/docker-build.sh
@@ -1,3 +1,2 @@
 docker build --tag text-captcha-task-manager -f dockerfiles/TaskManager.WithBuild.Dockerfile .
-
 docker build --tag text-captcha-text-ingest -f dockerfiles/TextIngest.WithBuild.Dockerfile .

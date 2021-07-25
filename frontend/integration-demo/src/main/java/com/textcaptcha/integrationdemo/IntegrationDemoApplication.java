34e74bd63d159fad075ef616a90715a89bd55c39
mc0239 <martin.cebular@gmail.com>
2021-07-25 13:09:15 +0200

    integration-demo: print openapi/swagger url on start

--- frontend/integration-demo/src/main/java/com/textcaptcha/integrationdemo/IntegrationDemoApplication.java
+++ frontend/integration-demo/src/main/java/com/textcaptcha/integrationdemo/IntegrationDemoApplication.java
@@ -3,14 +3,41 @@ package com.textcaptcha.integrationdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 @SpringBootApplication
 public class IntegrationDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntegrationDemoApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(IntegrationDemoApplication.class, args);
+
+        printLocalAddress(context);
+    }
+
+    private static void printLocalAddress(ConfigurableApplicationContext context) {
+        Environment env = context.getEnvironment();
+
+        if (!Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+            // Only print address on dev environment.
+            return;
+        }
+
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+
+        if (path == null || "/".equals(path)) {
+            path = "";
+        }
+
+        String address = "http://localhost:" + port + path;
+
+        System.out.println("Application started on address: " + address);
     }
 
 }

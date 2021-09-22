ea0f51f0d02fe484794586ac39b582b1907f40a9
mc0239 <martin.cebular@gmail.com>
2021-09-22 17:32:21 +0200

    backend: nothing

---
+++ backend/text-ingest/src/main/test/com.textcaptcha.textingest/SampleTest.java
@@ -0,0 +1,14 @@
+package com.textcaptcha.textingest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void isTrue() {
+        assertTrue(true);
+    }
+
+}
